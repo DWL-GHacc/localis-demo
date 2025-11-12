679a254d7afa12ba829e5eb063e85d96d09301ac
Saki Ota <saki@SakinoMacBook-Air.local>
2025-11-12 21:13:49 +1100

    addded more queries for each table

--- server/localis_data/routes/historical/index.js
+++ server/localis_data/routes/historical/index.js
@@ -67,4 +67,87 @@ router.get("/average_rates", async (req, res, next) => {
   }
 });
 
+router.get("/distinct_lgas_historical", async (req, res, next) => {
+  req.db
+    .from("historical")
+    .distinct("lga_name")
+    .orderBy("lga_name")
+    .then((rows) => {
+      res.json({ Error: false, Message: "success", Data: rows });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ Error: true, Message: "Error executing MySQL query" });
+    });
+});
+
+router.get("/monthly_occupancy_ADR_per_LGA", async (req, res, next) => {
+    req.db
+    .from("historical")
+    .select(
+      "lga_name",
+      req.db.raw("YEAR(hist_date) AS year"),
+      req.db.raw("MONTH(hist_date) AS month"),
+      req.db.raw("AVG(average_historical_occupancy) AS avg_occupancy"),
+      req.db.raw("AVG(average_daily_rate) AS avg_adr")
+    )
+    .groupBy(
+      "lga_name",
+      req.db.raw("YEAR(hist_date)"),
+      req.db.raw("MONTH(hist_date)")
+    )
+    .orderBy(["lga_name", "year", "month"])
+    .then((rows) => {   
+      res.json({ Error: false, Message: "success", Data: rows });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ Error: true, Message: "Error executing MySQL query" });
+    });
+});
+
+router.get("/single_LGA_histOcc_LOS", async (req, res, next) => {
+//   const  lga  = req.query;
+//   console.log("LGA selected", lga);
+
+const lga = "Gold Coast";  // For testing purpose
+// frontend request URL example:
+// http://localhost:3000/historical/single_LGA_histOcc_LOS?lga_name=Gold%20Coast
+
+  if(!lga){
+    return res.status(400).json({ Error: true, Message: "Missing lga_name" });
+  }
+
+  req.db
+    .from("historical as h")
+    .join("length_data as l", function () {
+      this.on("h.lga_name", "=", "l.lga_name").andOn(
+        "h.hist_date",
+        "=",
+        "l.date_length"
+      );
+    })
+    .select(
+      "h.lga_name",
+      req.db.raw("YEAR(h.hist_date) AS year"),
+      req.db.raw("MONTH(h.hist_date) AS month"),
+      req.db.raw("AVG(h.average_historical_occupancy) AS avg_occupancy"),
+      req.db.raw("AVG(h.average_daily_rate) AS avg_adr"),
+      req.db.raw("AVG(l.average_length_of_stay) AS avg_length_of_stay"),
+      req.db.raw("AVG(l.average_booking_window) AS avg_booking_window")
+    )
+    .where("h.lga_name", lga)
+    .groupBy("h.lga_name")
+    .groupByRaw("YEAR(h.hist_date), MONTH(h.hist_date)")
+    .orderBy(["year", "month"])
+    .then((rows) => {
+      res.json({ Error: false, Message: "success", Data: rows });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ Error: true, Message: "Error executing MySQL query" });
+    });
+});
+
+
 module.exports = router;
