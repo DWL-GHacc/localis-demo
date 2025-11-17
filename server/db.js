d8b998f105c4e7b84284ed5d4286ca046fff6e67
Saki Ota <saki@SakinoMacBook-Air.local>
2025-11-17 20:41:56 +1100

    change to commonJS to import knex

--- server/db.js
+++ server/db.js
@@ -1,16 +1,19 @@
 // server/db.js
-require('dotenv').config();
-const knex = require('knex');
+import dotenv from "dotenv";
+import knex from "knex";
 
-module.exports = knex({
-  client: 'mysql2',
+dotenv.config();
+
+const db = knex({
+  client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
   },
-  pool: { min: 0, max: 10 }
+  pool: { min: 0, max: 10 },
 });
 
+export default db;
